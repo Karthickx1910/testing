2538c85a4460648e7feb2c797be7e2b84f3640c6
Karthick <megamind@Karthicks-MacBook-Air.local>
2024-04-08 23:00:59 +0530

    frst commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import MyButton from './button';
+import AboutPage from './Pages/about';
+
+function App() {
+  return (
+    <div className="App">
+      <MyButton></MyButton>
+      <AboutPage/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Pages/about.js
@@ -0,0 +1,12 @@
+function AboutPage() {
+    return (
+      <>
+      {/* <img className="avatar" /> */}
+      <img className="avatar" />
+        <h1>About</h1>
+        <p>Hello there.<br />How do you do?</p>
+      </>
+    );
+  }
+
+  export default AboutPage;
